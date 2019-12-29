f66905f40139e222dd3ca911ea9aa51345184c91
builderb gamer <builderbgamer@gmail.com>
2019-12-29 13:04:00 +0000

    Major changes, tweaks and new features

---
+++ Gallery/wwwroot/javascript/scrollto.js
@@ -0,0 +1,19 @@
+﻿function scrollToAnchor(anchor) {
+    var selector = anchor || document.location.hash;
+    if (selector && selector.length > 1) {
+        var element = document.querySelector(selector);
+        if (element) {
+            var y = element.getBoundingClientRect().top + window.pageYOffset;
+            /* The following code updates the vertical scroll bar's offset for a standard Blazor Visual Studio template.
+               Update the code to get an offset that is suitable for your application.  */
+            y -= document.querySelector(".main .top-row").offsetHeight
+            window.scroll(0, y);
+        }
+    }
+    else {
+        window.scroll(0, 0);
+    }
+}
+function scrollToTop(pos) {
+    window.scroll(0, pos);
+}
