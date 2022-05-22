105d0bd4e44d8a9e2387bc7acf7e4bedea781b8d
Builderb <builderbgamer@gmail.com>
2022-05-22 23:14:27 +0100

    Context menu update

---
+++ Gallery/wwwroot/javascript/albumchanger.js
@@ -0,0 +1,15 @@
+﻿function KeyListen() {
+    document.addEventListener('keydown', logKey);
+
+    function logKey(e) {
+        if (e.keyCode == '65') {
+            var log = document.getElementsByClassName('rz-paginator-prev')[0];
+            log.click();
+        }
+        if (e.keyCode == '68') {
+            var log = document.getElementsByClassName('rz-paginator-next')[0];
+            log.click();
+        }
+    }
+
+}
